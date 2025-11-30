72101f404302663c523db90346ad6fffc5c91551
limoneno0 <145608011+limoneno0@users.noreply.github.com>
2025-11-30 17:44:08 -0300

    Update scriptfunc.js

--- src/scriptfunc.js
+++ src/scriptfunc.js
@@ -195,7 +195,7 @@ function checkAuth() {
 function sair() {
     localStorage.removeItem('clockin:token');
     localStorage.removeItem('clockin:user');
-    window.location.href = "../src/login.html";
+    window.location.href = "../src/login/login.html";
 }
 
 // --- INICIALIZAÇÃO ---
